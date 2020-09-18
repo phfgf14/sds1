55c65bc8a8ccb194e9acad65bfd3d75f42df2d0e
Paulo Henrique Fernandes <phenrique.fernandes@yahoo.com>
2020-09-18 19:05:16 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
-function App() {
+export default function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
-
-export default App;
