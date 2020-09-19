1c8cf7d9731218944490681d4006cf61a2f1a9fc
Paulo Henrique Fernandes <phenrique.fernandes@yahoo.com>
2020-09-19 11:54:19 -0300

    Criando pagina de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import axios from 'axios';
+import './styles.css';
+import { RecordsResponse } from './types';
+import { formatDate } from './Helpers';
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom';
+
+const BASE_URL = 'http://localhost:8080';
 
 export default function Records() {
+  const [recordsResponse, SetRecordsResponse] = React.useState<
+    RecordsResponse
+  >();
+
+  const [activePage, SetActivePage] = React.useState(0);
+
+  React.useEffect(() => {
+    axios
+      .get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+      .then((response) => SetRecordsResponse(response.data));
+  }, [activePage]);
+
+  const handlePageChange = (index: number) => {
+    SetActivePage(index);
+  };
+
   return (
     <div className="page-container">
-      <h1>Records</h1>
+      <div className="filters-container records-actions">
+        <Link to="/charts">
+          <button className="action-filters">VER GRÁFICOS</button>
+        </Link>
+      </div>
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANT</th>
+            <th>NOME</th>
+            <th>IDADE</th>
+            <th>PLATAFORMA</th>
+            <th>GÊNERO</th>
+            <th>TÍTULO DO GAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recordsResponse?.content.map(
+            ({ id, moment, name, age, gameTitle, gamePlatform, genreName }) => {
+              return (
+                <tr key={id}>
+                  <td>{formatDate(moment)}</td>
+                  <td>{name}</td>
+                  <td>{age}</td>
+                  <td className="text-secondary">{gamePlatform}</td>
+                  <td>{genreName}</td>
+                  <td className="text-primary">{gameTitle}</td>
+                </tr>
+              );
+            }
+          )}
+        </tbody>
+      </table>
+      <Pagination
+        totalPages={recordsResponse?.totalPages}
+        goToPage={handlePageChange}
+        activePage={activePage}
+      />
     </div>
   );
 }
